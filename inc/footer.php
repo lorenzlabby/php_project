293857681e0f18771a34bef0da9396e03bb71c7a
Lorenzo Montecalbo <31034982+lorenzlabby@users.noreply.github.com>
2018-08-27 00:18:04 +0300

    with static file

---
+++ inc/footer.php
@@ -0,0 +1,4 @@
+<script src="inc/static/js/jquery-3.3.1.min.js"></script>
+<script src="inc/static/js/boostrap.min.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php 
-    echo "Hello lorenzo";
+    require('vendor/autoload.php');
+    require('inc/header.php');
 ?>
+    <div class="container">
+        <h1>test with bootstrap</h1>
+    </div>
+
+<?php require('inc/footer.php'); ?>
